1fdf5ea9dd1b80ed888d0a55867e07efcea9d3af
RedDash16 <reddash16@icloud.com>
2024-01-15 18:50:51 +0000

    Kiseki renames

--- src/main/java/json/jayson/init/LMItems.java
+++ src/main/java/json/jayson/init/LMItems.java
@@ -19,7 +19,7 @@ public class LMItems {
 	
 	//Tech
     public static final Item IRON_STOP_SIGN = registerItem("iron_stop_sign", new Item(new FabricItemSettings()));
-    public static final Item DEFAULT_FLASHLIGHT = registerItem("flashlight", new Item(new FabricItemSettings()));
+    public static final Item DEFAULT_FLASHLIGHT = registerItem("lime_flashlight", new Item(new FabricItemSettings()));
 
     private static void addToLMTab(FabricItemGroupEntries entries) {
     }
