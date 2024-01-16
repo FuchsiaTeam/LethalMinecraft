10a1dd9c2971de78f92af70d907b83c046107288
Jayson <jaysonjson@web.de>
2024-01-16 14:02:52 +0100

    Update PlayerEntityMixin.java

--- src/main/java/json/jayson/mixin/PlayerEntityMixin.java
+++ src/main/java/json/jayson/mixin/PlayerEntityMixin.java
@@ -1,15 +1,15 @@
 package json.jayson.mixin;
 
 import com.google.common.collect.ImmutableList;
+import json.jayson.LM;
 import json.jayson.common.item.IAmScrapLoot;
 import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.Arm;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,12 +24,12 @@ public abstract class PlayerEntityMixin extends LivingEntity{
         super(entityType, world);
     }
 
-    @Inject(at = @At("RETURN"), method = "dropItem(Lnet/minecraft/item/ItemStack;Z)Lnet/minecraft/entity/ItemEntity;")
-    private void onItemDrop(CallbackInfoReturnable<ItemStack> ci) {
+    @Inject(at = @At("RETURN"), method = "dropItem(Lnet/minecraft/item/ItemStack;ZZ)Lnet/minecraft/entity/ItemEntity;")
+    private void onItemDrop(CallbackInfoReturnable<ItemEntity> ci) {
         if(ci.getReturnValue().getItem() instanceof IAmScrapLoot) {
-            ItemStack stack = ci.getReturnValue();
+            ItemStack stack = ci.getReturnValue().getStack();
         }
-        System.out.println(getPos().toString());
-        ci.setReturnValue(new ItemStack(Items.AIR));
+        LM.LOGGER.debug(getPos().toString() + " : 1");
+        ci.setReturnValue(new ItemEntity(new ItemStack(Items.AIR, getPos().x, getPos().y, getPos().z)));
     }
 }
