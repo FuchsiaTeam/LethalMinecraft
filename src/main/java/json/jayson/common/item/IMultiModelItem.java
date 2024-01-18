7d5f39d39153c7364fc9b397816f3944d233bd56
Jayson <jaysonjson@web.de>
2024-01-18 01:33:11 +0100

    I may be stupid

--- src/main/java/json/jayson/common/item/IMultiModelItem.java
+++ src/main/java/json/jayson/common/item/IMultiModelItem.java
@@ -1,7 +1,5 @@
 package json.jayson.common.item;
 
-import net.minecraft.util.Identifier;
-
 public interface IMultiModelItem {
 
    default String getModel() {
--- src/main/java/json/jayson/mixin/ItemRendererMixin.java
+++ src/main/java/json/jayson/mixin/ItemRendererMixin.java
@@ -1,13 +1,13 @@
 package json.jayson.mixin;
 
-import json.jayson.LM;
 import json.jayson.common.item.IMultiModelItem;
-import json.jayson.init.LMItems;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -18,7 +18,8 @@ public class ItemRendererMixin {
     @ModifyVariable(method = "renderItem*", at = @At(value = "HEAD"), argsOnly = true)
     public BakedModel getIronStopSignModel(BakedModel model, ItemStack itemStack, ModelTransformationMode mode) {
         if(itemStack.getItem() instanceof IMultiModelItem multiModelItem && mode != ModelTransformationMode.GUI) {
-            return ((ItemRendererAccessor) this).lm$getModels().getModelManager().getModel(new ModelIdentifier(LM.ID, multiModelItem.getModel().isEmpty() ? itemStack.getItem().toString() + "_model" : multiModelItem.getModel(), multiModelItem.getModelVariant()));
+            Identifier itemId = Registries.ITEM.getId(itemStack.getItem());
+            return ((ItemRendererAccessor) this).lm$getModels().getModelManager().getModel(new ModelIdentifier(itemId.getNamespace(), multiModelItem.getModel().isEmpty() ? itemId.getPath() + "_model" : multiModelItem.getModel(), multiModelItem.getModelVariant()));
         }
         return model;
     }
