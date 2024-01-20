5a5719d42f6a9f01f14a8a8e77f816acf03fc8aa
Jayson Hartmann <jaysonjson@web.de>
2024-01-20 23:24:38 +0000

    Update ScalingAlgorithm.java

--- src/main/java/json/jayson/ResolutionControl/ScalingAlgorithm.java
+++ src/main/java/json/jayson/ResolutionControl/ScalingAlgorithm.java
@@ -4,23 +4,15 @@ import net.minecraft.text.Text;
 import org.lwjgl.opengl.GL11;
 
 public enum ScalingAlgorithm {
-    NEAREST(Text.translatable("resolutioncontrol.settings.main.nearest"),
-            GL11.GL_NEAREST, GL11.GL_NEAREST_MIPMAP_NEAREST),
-    LINEAR(Text.translatable("resolutioncontrol.settings.main.linear"),
-            GL11.GL_LINEAR, GL11.GL_LINEAR_MIPMAP_NEAREST);
+    NEAREST(GL11.GL_NEAREST, GL11.GL_NEAREST_MIPMAP_NEAREST),
+    LINEAR(GL11.GL_LINEAR, GL11.GL_LINEAR_MIPMAP_NEAREST);
 
-    private final Text text;
     private final int id;
     private final int idMipped;
 
-    ScalingAlgorithm(Text text, int id, int idMipped) {
-        this.text = text;
+    ScalingAlgorithm(int id, int idMipped) {
         this.id = id;
         this.idMipped = idMipped;
-    }
-
-    public Text getText() {
-        return text;
     }
 
     public int getId(boolean mipped) {
