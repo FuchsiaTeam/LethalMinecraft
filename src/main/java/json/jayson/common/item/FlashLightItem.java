b6e85896e24fff65e0a8d9a33432116a9832ce0c
Jayson Hartmann <jaysonjson@web.de>
2024-01-20 23:18:13 +0000

    ef

--- src/main/java/json/jayson/common/item/FlashLightItem.java
+++ src/main/java/json/jayson/common/item/FlashLightItem.java
@@ -24,23 +24,32 @@ public class FlashLightItem extends Item {
     @Override
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         super.inventoryTick(stack, world, entity, slot, selected);
-        if(selected) {
-            Vec3d rotVec = entity.getRotationVector();
-            for (int i = 2; i < 10; i = i + 2) {
-                Vec3d lightPos3d = entity.getPos().add(0, 1, 0).add(rotVec.multiply(i));
-                BlockPos lightPos = new BlockPos((int) lightPos3d.x, (int) lightPos3d.y, (int) lightPos3d.z);
-                if (world.getBlockState(lightPos).getBlock().equals(Blocks.AIR)) {
-                    world.setBlockState(lightPos, Blocks.LIGHT.getDefaultState().with(LightBlock.LEVEL_15, 7), 0);
-                    LIGHTS.add(lightPos);
+        if(world.isClient) {
+            if (selected) {
+                for (BlockPos light : LIGHTS) {
+                    if (world.getBlockState(light).getBlock().equals(Blocks.LIGHT)) {
+                        world.setBlockState(light, Blocks.AIR.getDefaultState(), 0);
+                    }
                 }
-            }
-        } else {
-            for (BlockPos light : LIGHTS) {
-                if(world.getBlockState(light).getBlock().equals(Blocks.LIGHT)) {
-                    world.setBlockState(light, Blocks.AIR.getDefaultState(), 0);
+                LIGHTS.clear();
+
+                Vec3d rotVec = entity.getRotationVector();
+                for (int i = 2; i < 10; i = i + 2) {
+                    Vec3d lightPos3d = entity.getPos().add(0, 1, 0).add(rotVec.multiply(i));
+                    BlockPos lightPos = new BlockPos((int) lightPos3d.x, (int) lightPos3d.y, (int) lightPos3d.z);
+                    if (world.getBlockState(lightPos).getBlock().equals(Blocks.AIR)) {
+                        world.setBlockState(lightPos, Blocks.LIGHT.getDefaultState().with(LightBlock.LEVEL_15, 7), 0);
+                        LIGHTS.add(lightPos);
+                    }
                 }
+            } else {
+                for (BlockPos light : LIGHTS) {
+                    if (world.getBlockState(light).getBlock().equals(Blocks.LIGHT)) {
+                        world.setBlockState(light, Blocks.AIR.getDefaultState(), 0);
+                    }
+                }
+                LIGHTS.clear();
             }
-            LIGHTS.clear();
         }
     }
 }
