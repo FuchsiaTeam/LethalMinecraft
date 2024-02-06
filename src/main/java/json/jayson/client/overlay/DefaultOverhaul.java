ab9cab63adb1c8fa44eae5bddf744166abe0e64b
Jayson.json <jaysonjson@web.de>
2024-02-06 03:12:54 +0100

    Update DefaultOverhaul.java

--- src/main/java/json/jayson/client/overlay/DefaultOverhaul.java
+++ src/main/java/json/jayson/client/overlay/DefaultOverhaul.java
@@ -28,57 +28,62 @@ public class DefaultOverhaul implements HudRenderCallback {
             int x = (client.getWindow().getScaledWidth() / 2);
             int y = client.getWindow().getScaledHeight() / 2;
             /* DROP TEXT */
-            if(client.player.getMainHandStack().getItem() instanceof IAmScrapLoot) {
-                String dropText = "Drop " + client.player.getMainHandStack().getName().getString() + "  :  [" + client.options.dropKey.getBoundKeyLocalizedText().getString() + "]";
-                int dropTextLen = MinecraftClient.getInstance().textRenderer.getWidth(dropText);
-                drawContext.drawText(client.textRenderer, dropText, x * 2 - 12 - dropTextLen, y - 100, 0xFFFFFFFF, true);
-            }
-            /*          */
-
-
+            drawDropText(drawContext, x, y, client);
             /* HP; WEIGHT */
-            drawContext.drawTexture(PLAYER_TEX, x - 190, y - 100, 0, 0, 0, 32, 32, 32,32);
-            int healthPerc = (int)(client.player.getHealth() / client.player.getMaxHealth() * 100);
-            String health = String.valueOf(healthPerc);
-            int healthColor = 0xFF297D23;
-            if(healthPerc == 100) {
-                drawContext.drawText(client.textRenderer, health.substring(1), x - 195, y - 78, healthColor, true);
-                drawContext.drawText(client.textRenderer, "1", x - 203, y - 78, healthColor, true);
-            } else {
-                drawContext.drawText(client.textRenderer, health, x - 210, y - 87, healthColor, true);
-            }
-
+            drawHealthPlayer(drawContext, x, y, client);
             /* Now comes the stamina, I suck at this type of stuff so dont wonder lmao ~Jayson */
+            drawStaminaBar(drawContext,x,y);
+        }
+    }
 
 
-            --staminaTemp;
-            if(0 >= staminaTemp) {
-                staminaTemp = 100;
-            }
+    public void drawDropText(DrawContext drawContext, int x, int y, MinecraftClient client) {
+        if(client.player.getMainHandStack().getItem() instanceof IAmScrapLoot) {
+            String dropText = "Drop " + client.player.getMainHandStack().getName().getString() + "  :  [" + client.options.dropKey.getBoundKeyLocalizedText().getString() + "]";
+            int dropTextLen = MinecraftClient.getInstance().textRenderer.getWidth(dropText);
+            drawContext.drawText(client.textRenderer, dropText, x * 2 - 12 - dropTextLen, y - 100, 0xFFFFFFFF, true);
+        }
+    }
 
-            int staminaPerc = (int)(staminaTemp / 100.0 * 100.0f);
-            int staminaU = 0;
-            boolean staminaUpper = true;
-
-            if(99>= staminaPerc) {
-                staminaU = 115 - staminaPerc;
-            }
-
-            if(67>= staminaPerc) {
-                staminaU = 25;
-                staminaUpper = false;
-            }
-
-
+    public void  drawHealthPlayer(DrawContext drawContext, int x, int y, MinecraftClient client) {
+        drawContext.drawTexture(PLAYER_TEX, x - 190, y - 100, 0, 0, 0, 32, 32, 32,32);
+        int healthPerc = (int)(client.player.getHealth() / client.player.getMaxHealth() * 100);
+        String health = String.valueOf(healthPerc);
+        int healthColor = 0xFF297D23;
+        if(healthPerc == 100) {
+            drawContext.drawText(client.textRenderer, health.substring(1), x - 195, y - 78, healthColor, true);
+            drawContext.drawText(client.textRenderer, "1", x - 203, y - 78, healthColor, true);
+        } else {
+            drawContext.drawText(client.textRenderer, health, x - 210, y - 87, healthColor, true);
+        }
+    }
 
 
+    public void drawStaminaBar(DrawContext drawContext, int x, int y) {
+        /* THIS IS VERY HACKY AND NEEDS TOTALLY A REWRITE! */
+        --staminaTemp;
+        if(0 >= staminaTemp) {
+            staminaTemp = 100;
+        }
+
+        int staminaPerc = (int)(staminaTemp / 100.0 * 100.0f);
+        int staminaU = 0;
+        boolean staminaUpper = true;
+
+        if(99>= staminaPerc) {
+            staminaU = 115 - staminaPerc;
+        }
+
+        if(67>= staminaPerc) {
+            staminaU = 25;
+            staminaUpper = false;
+        }
             /*staminaTemp = 127;
             int stamina = (int)(staminaTemp / 100.0 * 39.0f);
             int staminaU = 59 - stamina;*/
-            drawContext.drawTexture(STAMINA_BAR_TEX, x - 212, y - 110 + staminaU, 0, staminaU, staminaUpper ? 64 : (int)(staminaTemp / 100.0 * 64.0f), 64, 64,128);
-            drawContext.drawTexture(STAMINA_BAR_TEX, x - 212, y - 110, 0, 0, (int)(staminaTemp / 100.0 * 32.0f), 64, 64,128);
-            drawContext.drawText(client.textRenderer, "0  lb", x - 160, y - 60, 0xFFA3691D, true);
-        }
+        drawContext.drawTexture(STAMINA_BAR_TEX, x - 212, y - 110 + staminaU, 0, staminaU, staminaUpper ? 64 : (int)(staminaTemp / 100.0 * 64.0f), 64, 64,128);
+        drawContext.drawTexture(STAMINA_BAR_TEX, x - 212, y - 110, 0, 0, (int)(staminaTemp / 100.0 * 32.0f), 64, 64,128);
+        drawContext.drawText(client.textRenderer, "0  lb", x - 160, y - 60, 0xFFA3691D, true);
     }
 
 }
